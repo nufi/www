ab48953d2a38554ae54d90ee62cb0cc468149a1d
Flaubert Moumi Wansi <1494111@cegeplimoilou.ca>
2016-04-09 16:41:15 -0400

    Signed-off-by: Flaubert Moumi Wansi <1494111@cegeplimoilou.ca>

---
+++ projets/TutorielJS/scripts/main.js
@@ -0,0 +1,17 @@
+var monBouton = document.querySelector('button');
+var monTitre = document.querySelector('h1');
+
+function définirNomUtilisateur() {
+  var monNom = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', monNom);
+  monTitre.textContent = 'Mozilla est cool, ' + monNom;
+}
+if(!localStorage.getItem('nom')) {
+  définirNomUtilisateur();
+} else {
+  var nomEnregistré = localStorage.getItem('nom');
+  monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré;
+}
+monBouton.onclick = function() {
+  définirNomUtilisateur();
+}
